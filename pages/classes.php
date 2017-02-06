927b7f61c1e1ccf5b10b03ee568c124cb4add696
Zach Howard <zach.d.howard@gmail.com>
2017-02-06 17:44:42 -0500

    Planning Classes and Modules
    Planning Classes and Modules

---
+++ pages/classes.php
@@ -0,0 +1,110 @@
+<?php
+
+// THESE ARE NOT FORMATTED CORRECTLY, THIS IS LOGIC AND PLANNING ONLY
+
+class User {
+  API[] {
+    keyID
+    vCode
+
+    updateInfo()
+  }
+
+  Character[] {
+
+    Info {
+      ID
+      name
+      avatar
+
+      corpID
+      corpName
+      corpLogo
+
+      allianceID
+      allianceName
+      allianceLogo
+
+      factionID
+      factionName
+      factionLogo
+
+      skillQueue[]
+
+      FWStats[]
+
+      standings[]
+      buddies[]
+
+      notifications[]
+
+      Wallet {
+        accountBalance
+
+        walletJournal[]
+        walletTransactions[]
+
+        getAccountBalance()
+        getJournal()
+        getTransactions()
+      }
+
+      Assets {
+        items[]
+
+        getAssets()
+        getAssetsForSystem()
+      }
+
+      Calendar {
+        events[]
+
+        getEvents();
+      }
+
+      Contract {
+        contracts[]
+
+        getContracts()
+        getBiddingContracts()
+      }
+
+      Industry {
+        jobsManufacture[]
+        jobsResearch[]
+
+        getManufacturingJobs()
+        getResearchJobs()
+      }
+
+      Mail {
+        mails[]
+        mailingLists[]
+
+        getMail()
+        getMailingList()
+        sendMail()
+      }
+
+      Market {
+        buyOrders[]
+        sellOrders[]
+        watchedItems[]
+
+        getMarketOrders()
+        getWatchedItems()
+      }
+    }
+
+    getRecentNotifications()
+
+  }
+
+  class Server {
+    status
+
+    checkStatus()
+  }
+}
+
+?>
---
+++ pages/modules.php
@@ -0,0 +1,110 @@
+<?php
+
+  // EVERY MODULE SHOULD HAVE A SINGLE/ALL CHARACTER OPTION
+
+  /////
+  // Modules to Design
+  // - Required Input
+  //////////
+
+  /////
+  // Account Balance
+  //  - characterID
+  //////////
+
+  /////
+  // Asset List Per System
+  //  - characterID, systemID
+  //////////
+
+  /////
+  // Calendar Events
+  // -
+  //////////
+
+  /////
+  // Character Sheet
+  // - characterID
+  //////////
+
+  /////
+  // Online Contacts
+  // -
+  //////////
+
+  /////
+  // Contracts per Type
+  // -
+  //////////
+
+  /////
+  // Contract Bids
+  // -
+  //////////
+
+  /////
+  // FW Stats
+  // -
+  //////////
+
+  /////
+  // Industry Jobs
+  // -
+  //////////
+
+  /////
+  // Singular Mailing List
+  // -
+  //////////
+
+  /////
+  // Market Orders
+  // -
+  //////////
+
+  /////
+  // Recent Notifications
+  // -
+  //////////
+
+  /////
+  // Research
+  // -
+  //////////
+
+  /////
+  // Skill Queue
+  // -
+  //////////
+
+  /////
+  //Standings
+  // -
+  //////////
+
+  /////
+  // Wallet Journal
+  // -
+  //////////
+
+  /////
+  // Wallet Transactions
+  // -
+  //////////
+
+  /////
+  // Server Status
+  // -
+  //////////
+
+  /////
+  // Recent Mail (Not Mailing List)
+  // -
+  //////////
+
+  /////
+  // Send Mail
+  // -
+  //////////
+
+?>
