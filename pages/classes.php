f84c23a83b60d23b6bf1b5d643743a73b274e76c
Zach Howard <zach@zach.d.howard@gmail.com>
2017-02-07 01:34:44 -0500

    Planning
    More planning

--- pages/classes.php
+++ pages/classes.php
@@ -3,6 +3,45 @@
 // THESE ARE NOT FORMATTED CORRECTLY, THIS IS LOGIC AND PLANNING ONLY
 
 class User {
+	
+	public function __construct() {
+		//TODO Load keyID from db
+		//TODO Load vCode from db
+	}
+	
+	private class API[] = {
+		private $keyID = '';
+		private $vCode = '';
+		private Character $chars[2];
+		
+		private function updateInfo(); // needs to return true if successful
+		
+		public function __get() {
+			if ( updateInfo() ) {
+				return $this;
+			} else {
+				die('Update Failed');
+			}
+		}
+		
+	}
+	
+	private class Character[] = {
+		
+	}
+	
+	public function updateInfo() {
+		try {
+			// TODO populate User's Character
+		} catch (Exception $e) {
+			return false;
+		}
+		
+		return true;
+	}
+}
+
+class oldUser {
   API[] {
     keyID
     vCode
